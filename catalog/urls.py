391220e4931368c2f0d31d59e2fde95498cb23de
Chen Ningshuang <cnsapril@gmail.com>
2016-08-23 14:32:59 +0800

    Initial commit

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from catalog.views import index, show_category, show_product
+
+urlpatterns = [
+    url(r'^$', index, {'template_name': 'catalog/index.html'}, name='catalog_home'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', show_category, {'template_name': 'catalog/category.html'},
+        name='catalog_category'),
+    url(r'^product/(?P<product_slug>[-\w]+)/$', show_product, {'template_name': 'catalog/product.html'},
+        name='catalog_product'),
+]
